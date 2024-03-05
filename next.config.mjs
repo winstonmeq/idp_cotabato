9aa23ef1ae6748b378b262726d3c91c4525c8840
winstonmeq <winstonmequila@gmail.com>
2024-03-05 21:13:01 +0800

    try again update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,13 @@ const nextConfig = {
 
     env: {
              
-        //LOCAL_URL: 'http://localhost:3000',        
+        //LOCAL_URL: 'http://localhost:3000',   
+
         LOCAL_URL: 'https://idpcotabato.vercel.app',
 
+        NEXTAUTH_URL: 'https://idpcotabato.vercel.app',
+
+        NEXTAUTH_SECRET: 'v7COYqKpEdnCbd5aISAw9BxjupOLKYCgBVZ2kwusMNs='
         
         
      },
